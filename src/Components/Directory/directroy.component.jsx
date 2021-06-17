d0399819e096101a3a412710308aca6147f17f90
ena <ena@ex.com>
2021-06-17 23:01:53 +0600

    pushing imageURL second time

--- src/Components/Directory/directroy.component.jsx
+++ src/Components/Directory/directroy.component.jsx
@@ -34,7 +34,7 @@ class Directory extends React.Component{
           },
           {
             title: 'mens',
-            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            imageURL: 'https://i.ibb.co/R70vBrQ/men.png',
             size: 'large',
             id: 5,
             linkUrl: ''
