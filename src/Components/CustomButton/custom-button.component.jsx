36e99ff696cf6fd158ae0c76aee1ca69f747b8c3
ena <ena@ex.com>
2021-06-17 22:46:48 +0600

    Updates

--- src/Components/CustomButton/custom-button.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './custom-button.styles.scss';
-
-const CustomButton = ({children,isGoogleSignIn,...otherProps})=> (
-    <button className={`${isGoogleSignIn ? "sign-in-google" : ""} custom-button`} {...otherProps}>
-        {children}
-    </button>
-);
-export default CustomButton;
