370b23a1baf4080c8df85a6d43563230bdc17e3b
ena <ena@ex.com>
2021-07-27 23:01:18 +0600

    session storage by using redux-persist library

--- src/Redux/root.reducer.js
+++ src/Redux/root.reducer.js
@@ -1,7 +1,17 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 import CartReducer from './cart/cart.reducer';
-export default combineReducers({
+
+ const persistConfig = {
+    key : 'root',
+    storage,
+    whitelist : ['cart']
+}
+
+ const rootReducers = combineReducers({
     user: userReducer,
     cart: CartReducer
 });
+export default persistReducer(persistConfig,rootReducers);
